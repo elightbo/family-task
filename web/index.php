c17e52d841ae09ae40983475cdb76bcc77b8de0e
elightbo <me@ericlightbody.com>
2015-10-30 06:36:54 -0500

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
