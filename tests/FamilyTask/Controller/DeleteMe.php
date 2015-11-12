c1ab63bec413c856bea441d5540591d8c421eda0
elightbo <me@ericlightbody.com>
2015-11-12 06:50:04 -0600

    Sets up travis and testing namespace.

---
+++ tests/FamilyTask/Controller/DeleteMe.php
@@ -0,0 +1,13 @@
+<?php
+namespace FamilyTask\Test;
+
+/**
+ * Delete me
+ */
+class DeleteMeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueStillEqualsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 require_once __DIR__.'/../app/bootstrap.php';
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->add('FamilyTask\\Test\\', __DIR__);
+
 $app['debug'] = true;
 unset($app['exception_handler']);
