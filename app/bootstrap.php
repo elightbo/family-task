acf69d628957bebab1b5d7a58231aef8cdd36901
Eric Lightbody <me@ericlightbody.com>
2015-11-20 08:56:40 -0500

    More messing around with testing.

--- app/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__.'/../vendor/autoload.php';
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-$app->post('/family', 'FamilyTask\Controller\FamilyController::createAction');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/../app/bootstrap.php';
+require_once __DIR__.'/../app/app.php';
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('FamilyTask\\Test\\', __DIR__);
